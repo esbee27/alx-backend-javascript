225382f9cd478e89754b3eb2b0b04573c5023bd3
Blessing <ogorblessing96@gmail.com>
2024-11-30 17:02:51 +0100

    Corrected task 6

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
 process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
+  const chunk = process.stdin.read();
     if (chunk !== null) {
-	process.stdout.write(`Your name is: ${chunk}`);
+	  process.stdout.write(`Your name is: ${chunk}`);
     }
 });
 
